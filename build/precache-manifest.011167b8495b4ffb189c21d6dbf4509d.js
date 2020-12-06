c3435461240306cc300a65983cb424c4de0db67c
DUKEadiaN <71949237+DUKEadiaN@users.noreply.github.com>
2020-12-07 00:55:36 +0530

    Add files via upload

---
+++ build/precache-manifest.011167b8495b4ffb189c21d6dbf4509d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b055b9b773524cdc4c8990105b99a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1854e09ef3216709b1a0",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1854e09ef3216709b1a0",
+    "url": "/static/js/main.db4cf5ce.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
